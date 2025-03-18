7df6d4115aaa12b42e292c17910216e558c3f46a
Rennifer Teixeira <rennifer10@gmail.com>
2025-03-17 21:46:39 -0300

    feat: primeiro test

---
+++ js-debugging/ex.1.js
@@ -0,0 +1,17 @@
+function squareDigits(num) {
+  return Number(
+    String(num)
+      .split("")
+      .map((digit) => digit ** 2)
+      .join("")
+  );
+}
+console.log(squareDigits(9119)); // 811181
+console.log(squareDigits(765)); // 493625
+
+fdsfsd;
+/*Convertemos o número em uma string para podermos manipular cada dígito individualmente.
+Usamos .split('') para transformar a string em um array de caracteres.
+Utilizamos .map(digit => digit ** 2) para elevar cada dígito ao quadrado.
+.join('') para unir os valores em uma string novamente.
+Number(...) para converter de volta para um número inteiro.*/
